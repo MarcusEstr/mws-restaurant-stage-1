3272ae36ec22a9e76fd7ed456c489834dad26080
MarcusEstr <estrada.marcus@gmail.com>
2018-10-22 19:02:50 -0700

    Adding some comments to sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
 const applicationName = 'RestaurantReviews';
-const cacheName = applicationName + "-v.0.8";
+const cacheName = applicationName + "-v.1.0";
 
 let cacheArray = [cacheName];
 
 //Cache assets when Service Worker is installed on browser.
 self.addEventListener('install', function(event) {
-
 	event.waitUntil(
-
+		//Provide listed files to browser for caching.
 		caches.open(cacheName).then(function(cache) {
-
 			return cache.addAll(
 				//All site content that we want cached should be here:
 				['/', '/restaurant.html', 
@@ -19,36 +17,24 @@ self.addEventListener('install', function(event) {
 				'/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg',
 				'/data/restaurants.json', '/favicon.ico']
 			);
-
 		})
-
 	);
-
 });
 
-//Delete a previous cache in browser:
+//Delete a previous cache in browser, if one exists:
 self.addEventListener('activate', function(event) {
-
 	event.waitUntil(
-
 		caches.keys().then(function(cacheNames) {
-
 			return Promise.all(
-
-				cacheNames.filter(function(cacheName) {
-
+				//Look and see if the cache name is the same as the current version (ie: v.1.0)
+				cacheNames.filter(function(cacheName) { //Return cache...
 					return cacheName.startsWith(applicationName) && !cacheArray.includes(cacheName);
-
-				}).map(function(cacheName) {
+				}).map(function(cacheName) { //... or delete the cache from browswer.
 					return caches.delete(cacheName);
 				})
-
 			);
-
 		})
-
 	);
-
 });
 
 
